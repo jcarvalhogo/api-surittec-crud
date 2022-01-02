b997e2fbe386219dae39f683ecb5a83a50124abf
Carvalho <jose.a.carvalho@capgemini.com>
2022-01-02 17:11:27 -0300

    Criado o projeto spring api

---
+++ src/main/java/br/com/jcarvalho/api/surittec/SurittecApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jcarvalho.api.surittec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurittecApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurittecApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/jcarvalho/api/surittec/SurittecApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jcarvalho.api.surittec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurittecApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
